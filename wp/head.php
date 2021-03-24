54377738d04ae9f332c633fa9f3b83cfaa387006
GUNAWAN18ID <44337431+GUNAWAN18ID@users.noreply.github.com>
2021-03-25 04:02:28 +0700

    Add files via upload

---
+++ wp/head.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>X-SERVER | Semua Ada Untukmu</title>
+<link rel="stylesheet" href="https://getbootstrap.com/docs/4.1/dist/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<script src="https://getbootstrap.com/docs/4.1/dist/js/bootstrap.min.js"></script>
+</script src="https://getbootstrap.com/docs/4.1/assets/js/vendor/popper.min.js"></script>
+<style>
+/* Show it is fixed to the top */
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: black;
+    color: white;
+    text-align: center;
+}
+.btn-primary-outline {
+    background-color: transparent;
+    border-color: red;
+    box-shadow: none
+}
+/* Show it is fixed to the top */
+body {
+  min-height: 75rem;
+  padding-top: 4.5rem;
+  color:white;
+}
+</style>
+</head> 
+
+<body>
+	
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+         <a class="navbar-brand" href="#">X-SERVER</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="//fb.me/basmal109">Contact Me</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="//fdci-indonesia.web.id">Blog</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
